67a13fc61b86c8fd8ba2d64ca82a340743f393c2
maxogden <max@maxogden.com>
2011-04-28 16:47:52 -0700

    switch to ruby because node mail parsing sucks

---
+++ gmail/fetch_data.rb
@@ -0,0 +1,73 @@
+require 'mail'
+load 'message_formatter.rb'
+module GmailArchiver
+  class FetchData
+    attr_accessor :seqno, :uid, :envelope, :rfc822, :size, :flags
+
+    def initialize(x)
+      @seq = x.seqno
+      @uid = x.attr['UID']
+      @message_id = x.attr["MESSAGE-ID"]
+      @envelope = x.attr["ENVELOPE"]
+      @size = x.attr["RFC822.SIZE"] # not sure what units this is
+      @flags = x.attr["FLAGS"]  # e.g. [:Seen]
+      @rfc822 = x.attr['RFC822']
+      @mail = Mail.new(x.attr['RFC822'])
+    end
+
+    def subject
+      envelope.subject
+    end
+
+    def sender
+      envelope.from.first
+    end
+
+    def in_reply_to
+      envelope.in_reply_to 
+    end
+
+    def message_id
+      envelope.message_id
+    end
+
+    # http://www.ruby-doc.org/stdlib/libdoc/net/imap/rdoc/classes/Net/IMAP.html
+    #
+
+    def message
+      formatter = MessageFormatter.new(@mail)
+      message_text = <<-EOF
+#{format_headers(formatter.extract_headers)}
+
+#{formatter.process_body}
+EOF
+    end
+
+    def format_parts_info(parts)
+      lines = parts.select {|part| part !~ %r{text/plain}}
+      if lines.size > 0
+        "\n#{lines.join("\n")}"
+      end
+    end
+
+    def format_headers(hash)
+      lines = []
+      hash.each_pair do |key, value|
+        if value.is_a?(Array)
+          value = value.join(", ")
+        end
+        lines << "#{key.gsub("_", '-')}: #{value}"
+      end
+      lines.join("\n")
+    end
+
+  end
+end
+
+# envelope.from # array
+# envelope.to # array
+# address_struct.name, mailbox, host , join @
+# envelope.date
+# envelope.subject
+# subject = Mail::Encodings.unquote_and_convert_to((envelope.subject || ''), 'UTF-8')
+#
---
+++ gmail/imap_client.rb
@@ -0,0 +1,83 @@
+# encoding: UTF-8
+require 'timeout'
+require 'yaml'
+require 'mail'
+require 'net/imap'
+require 'time'
+load 'fetch_data.rb'
+
+module GmailArchiver
+  class ImapClient
+    attr_accessor :max_seqno 
+    def initialize(config)
+      @username, @password = config['username'], config['password']
+      @imap_server = config['server'] || 'imap.gmail.com'
+      @imap_port = config['port'] || 993
+    end
+
+    def log s
+      if s.is_a?(Net::IMAP::TaggedResponse)
+        $stderr.puts s.data.text
+      else
+        $stderr.puts s
+      end
+    end
+
+    def with_open
+      @imap = Net::IMAP.new(@imap_server, @imap_port, true, nil, false)
+      log @imap.login(@username, @password)
+      list_mailboxes
+      yield self
+    ensure
+      close
+    end
+
+    def close
+      log "Closing connection"
+      Timeout::timeout(5) do
+        @imap.close rescue Net::IMAP::BadResponseError
+        @imap.disconnect rescue IOError
+      end
+    rescue Timeout::Error
+      log "Attempt to close connection timed out"
+    end
+
+    def select_mailbox(mailbox)
+      log @imap.select(mailbox)
+      @mailbox = mailbox
+    end
+
+    # TODO skip drafts and spam box and all box 
+    def list_mailboxes
+      log 'loading mailboxes...'
+      @mailboxes = (@imap.list("", "*") || []).select {|struct| struct.attr.none? {|a| a == :Noselect}}. map {|struct| struct.name}.uniq
+      log "Loaded mailboxes: #{@mailboxes.inspect}"
+    end
+
+    def archive_messages(opts = {})
+      opts = {range: (0..-1), per_slice: 10}.merge(opts)
+      uids = @imap.uid_search('ALL')
+      log "Got UIDs for #{uids.size} messages" 
+      range = uids[opts[:range]]
+      puts range.inspect
+      range.each_slice(opts[:per_slice]) do |uid_set|
+        @imap.uid_fetch(uid_set, ["FLAGS", 'ENVELOPE', "RFC822", "RFC822.SIZE", 'UID']).each do |x|
+          yield FetchData.new(x)
+        end
+      end
+    end
+  end
+end
+
+# THIS FOR TESTING ONLY
+config = YAML::load File.read(File.expand_path('~/.vmailrc'))
+imap = GmailArchiver::ImapClient.new(config)
+
+imap.with_open do |imap|
+  ['INBOX', '[Gmail]/Important'].each do |mailbox|
+    imap.select_mailbox mailbox
+    imap.archive_messages() do |fetch_data|
+      p fetch_data
+    end
+  end
+end
